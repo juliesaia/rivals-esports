c1450aa9c53067954819c3efb6c2d8acb7e71e69
UniversePenguin <legobobcat@gmail.com>
2023-01-05 22:40:23 -0500

    rocs loads now

--- server/constants.ts
+++ server/constants.ts
@@ -1,82 +1,348 @@
 export const allRCSMajors = [
-    "tournament/na-rcs-week-1/event/rivals-of-aether",
-    "tournament/na-rcs-week-2/event/rivals-of-aether",
-    "tournament/na-rcs-week-3/event/rivals-of-aether",
-    "tournament/garden-of-gods-a-rivals-of-aether-national/event/rivals-of-aether-singles",
-    "tournament/na-rcs-week-4/event/rivals-of-aether",
-    "tournament/na-rcs-week-5/event/rivals-of-aether",
-    "tournament/na-rcs-week-6/event/rivals-of-aether",
-    "tournament/guts-4-game-underground-tournament-spectacular-4/event/rivals-of-aether-singles",
-    "tournament/na-rcs-week-7/event/rivals-of-aether",
-    "tournament/rewired-2016-1/event/rivals-of-aether",
-    "tournament/na-rcs-week-8/event/rivals-of-aether",
-    "tournament/na-rcs-week-9/event/rivals-of-aether",
-    "tournament/na-rcs-week-10/event/rivals-of-aether",
-    "tournament/na-rcs-week-11/event/rivals-of-aether",
-    "tournament/na-rcs-week-12/event/rivals-of-aether",
-    "tournament/genesis-4/event/rivals-of-aether-singles",
-    "tournament/na-rcs-season-3-september-monthly/event/rivals-singles",
-    "tournament/road-to-shine-rivals/event/qualifier-1-may-20th",
-    "tournament/the-bigger-balc/event/rivals-of-aether-singles",
-    "tournament/road-to-shine-rivals/event/qualifier-2-june-10th",
-    "tournament/na-rcs-july-monthly/event/rivals-of-aether-singles",
-    "tournament/low-tier-city-5/event/rivals-of-aether-singles",
-    "tournament/rivals-super-smash-con/event/rivals-of-aether-singles",
-    "tournament/na-rcs-august-monthly/event/rivals-of-aether-singles",
-    "tournament/shine-2017/event/rivals-of-aether-singles",
-    "tournament/na-rcs-september-monthly/event/rivals-of-aether-singles",
-    "tournament/heat-wave/event/rivals-of-aether-singles",
-    "tournament/gametyrant-expo-2017/event/rivals-of-aether",
-    "tournament/na-rcs-october-monthly/event/rivals-of-aether-singles",
-    "tournament/na-rcs-november-monthly/event/rivals-of-aether-singles",
-    "tournament/na-rcs-december-monthly/event/rivals-of-aether-singles",
-    "tournament/genesis-5/event/rivals-of-aether",
-    "tournament/na-rcs-season-3-may-monthly-2/event/rivals-singles",
-    "tournament/smash-n-splash-4/event/rivals-of-aether-singles",
-    "tournament/na-rcs-season-3-june-monthly/event/rivals-singles",
-    "tournament/2gg-hyrule-saga/event/rivals-of-aether",
-    "tournament/na-rcs-season-3-july-monthly/event/rivals-singles",
-    "tournament/first-impact-2018/event/rivals-of-aether-singles",
-    "tournament/rivals-at-super-smash-con-2018/event/rivals-of-aether-singles",
-    "tournament/shine-2018/event/rivals-of-aether-singles",
-    "tournament/na-rcs-season-3-august-monthly/event/rivals-singles",
-    "tournament/na-rcs-season-3-october-monthly/event/rivals-singles",
-    "tournament/heat-wave-2/event/rivals-of-aether-singles",
-    "tournament/gametyrant-expo-2018/event/rivals-of-aether-1v1",
-    "tournament/na-rcs-season-3-november-monthly/event/rivals-singles",
-    "tournament/na-rcs-season-3-december-monthly/event/rivals-singles",
-    "tournament/don-t-park-on-the-grass-2018-1/event/rivals-of-aether-singles",
-    "tournament/genesis-6/event/rivals-of-aether-singles",
-    "tournament/smash-n-splash-5/event/rivals-of-aether-singles-1-000-pot-bonus",
-    "tournament/na-rcs-season-4-july-online-major/event/rivals-singles",
-    "tournament/rivals-of-aether-at-evo-2019/event/rivals-singles",
-    "tournament/indie-showcase-super-smash-con-2019/event/rivals-singles",
-    "tournament/glitch-7-minus-world/event/rivals-singles",
-    "tournament/heat-wave-3/event/rivals-of-aether-singles",
-    "tournament/dreamhack-atlanta-2019/event/rivals-1v1",
-    "tournament/na-rcs-season-4-december-online-major/event/rivals-singles",
-    "tournament/genesis-7-1/event/rivals-of-aether-singles",
-    "tournament/na-rocs-finals/event/rivals-of-aether-singles",
-    "tournament/rcs-season-5-fire/event/rivals-of-aether-singles",
-    "tournament/rcs-season-5-air/event/rivals-of-aether-singles",
-    "tournament/rcs-season-5-earth/event/rivals-of-aether-singles",
-    "tournament/rcs-season-5-water/event/rivals-of-aether-singles",
-    "tournament/na-rcs-finals/event/rivals-of-aether-singles",
-    "tournament/riptide-3/event/rivals-of-aether-singles",
-    "tournament/rcs-season-6-hot-springs/event/rivals-of-aether-singles",
-    "tournament/rcs-season-6-melody/event/rivals-of-aether-singles",
-    "tournament/heat-wave-4/event/rivals-of-aether-singles",
-    "tournament/rcs-season-6-fireworks-1/event/rivals-of-aether-singles",
-    "tournament/rcs-season-6-crystal/event/rivals-of-aether-singles",
-    "tournament/genesis-8/event/rivals-of-aether-singles",
-    "tournament/gote-4thekids-7/event/rivals-of-aether-singles",
-    "tournament/na-rcs-season-7-june-online-major/event/rivals-of-aether-singles",
-    "tournament/double-down-2022/event/rivals-of-aether-singles",
-    "tournament/indie-showcase-ssc-2022/event/rivals-singles",
-    "tournament/riptide-2022/event/rivals-of-aether-singles",
-    "tournament/the-big-house-10/event/rivals-of-aether-singles",
-    "tournament/heat-wave-5/event/rivals-of-aether-singles",
-    "tournament/na-rcs-season-7-december-online-major/event/rivals-of-aether-singles",
+    {
+        url: "tournament/rcs-prologue/event/rivals-of-aether",
+        season: 0,
+    },
+    {
+        url: "tournament/na-rcs-week-1/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-week-2/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-week-3/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/garden-of-gods-a-rivals-of-aether-national/event/rivals-of-aether-singles",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-week-4/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-week-5/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-week-6/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/guts-4-game-underground-tournament-spectacular-4/event/rivals-of-aether-singles",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-week-7/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/rewired-2016-1/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-week-8/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-week-9/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-week-10/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-week-11/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-week-12/event/rivals-of-aether",
+        season: 1,
+    },
+    {
+        url: "tournament/genesis-4/event/rivals-of-aether-singles",
+        season: 1,
+    },
+    {
+        url: "tournament/na-rcs-season-3-september-monthly/event/rivals-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/road-to-shine-rivals/event/qualifier-1-may-20th",
+        season: 2,
+    },
+    {
+        url: "tournament/the-bigger-balc/event/rivals-of-aether-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/road-to-shine-rivals/event/qualifier-2-june-10th",
+        season: 2,
+    },
+    {
+        url: "tournament/na-rcs-july-monthly/event/rivals-of-aether-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/low-tier-city-5/event/rivals-of-aether-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/rivals-super-smash-con/event/rivals-of-aether-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/na-rcs-august-monthly/event/rivals-of-aether-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/shine-2017/event/rivals-of-aether-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/na-rcs-september-monthly/event/rivals-of-aether-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/heat-wave/event/rivals-of-aether-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/gametyrant-expo-2017/event/rivals-of-aether",
+        season: 2,
+    },
+    {
+        url: "tournament/na-rcs-october-monthly/event/rivals-of-aether-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/na-rcs-november-monthly/event/rivals-of-aether-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/na-rcs-december-monthly/event/rivals-of-aether-singles",
+        season: 2,
+    },
+    {
+        url: "tournament/genesis-5/event/rivals-of-aether",
+        season: 2,
+    },
+    {
+        url: "tournament/na-rcs-season-3-may-monthly-2/event/rivals-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/smash-n-splash-4/event/rivals-of-aether-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/na-rcs-season-3-june-monthly/event/rivals-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/2gg-hyrule-saga/event/rivals-of-aether",
+        season: 3,
+    },
+    {
+        url: "tournament/na-rcs-season-3-july-monthly/event/rivals-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/first-impact-2018/event/rivals-of-aether-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/rivals-at-super-smash-con-2018/event/rivals-of-aether-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/elemental-rivalries/event/rivals-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/shine-2018/event/rivals-of-aether-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/na-rcs-season-3-august-monthly/event/rivals-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/na-rcs-season-3-october-monthly/event/rivals-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/heat-wave-2/event/rivals-of-aether-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/gametyrant-expo-2018/event/rivals-of-aether-1v1",
+        season: 3,
+    },
+    {
+        url: "tournament/na-rcs-season-3-november-monthly/event/rivals-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/na-rcs-season-3-december-monthly/event/rivals-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/don-t-park-on-the-grass-2018-1/event/rivals-of-aether-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/genesis-6/event/rivals-of-aether-singles",
+        season: 3,
+    },
+    {
+        url: "tournament/smash-n-splash-5/event/rivals-of-aether-singles-1-000-pot-bonus",
+        season: 4,
+    },
+    {
+        url: "tournament/na-rcs-season-4-july-online-major/event/rivals-singles",
+        season: 4,
+    },
+    {
+        url: "tournament/rivals-of-aether-at-evo-2019/event/rivals-singles",
+        season: 4,
+    },
+    {
+        url: "tournament/indie-showcase-super-smash-con-2019/event/rivals-singles",
+        season: 4,
+    },
+    {
+        url: "tournament/glitch-7-minus-world/event/rivals-singles",
+        season: 4,
+    },
+    {
+        url: "tournament/heat-wave-3/event/rivals-of-aether-singles",
+        season: 4,
+    },
+    {
+        url: "tournament/dreamhack-atlanta-2019/event/rivals-1v1",
+        season: 4,
+    },
+    {
+        url: "tournament/na-rcs-season-4-december-online-major/event/rivals-singles",
+        season: 4,
+    },
+    {
+        url: "tournament/genesis-7-1/event/rivals-of-aether-singles",
+        season: 4,
+    },
+    {
+        url: "tournament/rocs-1-online-major/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/rocs-2-online-major/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/rocs-3-online-major/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/rocs-4-online-major/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/rocs-5-online-major/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/rocs-6-online-major/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/na-rocs-finals/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/rcs-season-5-fire/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/rcs-season-5-air/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/rcs-season-5-earth/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/rcs-season-5-water/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/na-rcs-finals/event/rivals-of-aether-singles",
+        season: 5,
+    },
+    {
+        url: "tournament/riptide-3/event/rivals-of-aether-singles",
+        season: 6,
+    },
+    {
+        url: "tournament/rcs-season-6-hot-springs/event/rivals-of-aether-singles",
+        season: 6,
+    },
+    {
+        url: "tournament/rcs-season-6-melody/event/rivals-of-aether-singles",
+        season: 6,
+    },
+    {
+        url: "tournament/heat-wave-4/event/rivals-of-aether-singles",
+        season: 6,
+    },
+    {
+        url: "tournament/rcs-season-6-fireworks-1/event/rivals-of-aether-singles",
+        season: 6,
+    },
+    {
+        url: "tournament/rcs-season-6-crystal/event/rivals-of-aether-singles",
+        season: 6,
+    },
+    {
+        url: "tournament/genesis-8/event/rivals-of-aether-singles",
+        season: 6,
+    },
+    {
+        url: "tournament/gote-4thekids-7/event/rivals-of-aether-singles",
+        season: 7,
+    },
+    {
+        url: "tournament/na-rcs-season-7-june-online-major/event/rivals-of-aether-singles",
+        season: 7,
+    },
+    {
+        url: "tournament/double-down-2022/event/rivals-of-aether-singles",
+        season: 7,
+    },
+    {
+        url: "tournament/indie-showcase-ssc-2022/event/rivals-singles",
+        season: 7,
+    },
+    {
+        url: "tournament/riptide-2022/event/rivals-of-aether-singles",
+        season: 7,
+    },
+    {
+        url: "tournament/the-big-house-10/event/rivals-of-aether-singles",
+        season: 7,
+    },
+    {
+        url: "tournament/heat-wave-5/event/rivals-of-aether-singles",
+        season: 7,
+    },
+    {
+        url: "tournament/na-rcs-season-7-december-online-major/event/rivals-of-aether-singles",
+        season: 7,
+    },
 ];
 
 export const character_dict = {
@@ -119,10 +385,6 @@ export const unknownPlayers = [
         tag: "SisterEden",
         startggID: "70557ea3",
     },
-    // {
-    //     tag: "Gaming",
-    //     startggID: "d023dd93",
-    // },
     {
         tag: "Roxol",
         startggID: "a8f3fa41",
